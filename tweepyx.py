6ea44e9ffe431b0186be51a7cc4e62d146c7dca5
Tom <larsenthomasj@gmail.com>
2020-08-07 20:04:00 -0400

    try to prevent response 429

--- tweepyx.py
+++ tweepyx.py
@@ -97,7 +97,7 @@ class AyxPlugin:
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_secret)
 
-        api = tweepy.API(auth)
+        api = tweepy.API(auth, wait_on_rate_limit=True)
 
         for raw_tweet in tweepy.Cursor(api.search, q=self.Query, tweet_mode="extended").items():
             creator.reset()
